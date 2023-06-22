be1ab353da23b64d9743e50a673ad1620494ef39
lashkaryrad <lashkaryrad@gmail.com>
2023-06-22 21:15:03 +0330

    FIX env DIR

--- src/index.sh
+++ src/index.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 ## Environment Variables 
-# env="../env/.env"
-API_SITE=$( sed "s/#.*//; s/api=*//; s/Key.*.//; s/'//g; /^$/ d" .env ) 
-API_KEY=$( sed "s/#.*//; /https:/ d;s/api....//g; s/'//g; /^$/ d" .env )
+env="../env/.env"
+API_SITE=$( sed "s/#.*//; s/api=*//; s/Key.*.//; s/'//g; /^$/ d" $env ) 
+API_KEY=$( sed "s/#.*//; /https:/ d;s/api....//g; s/'//g; /^$/ d" $env )
