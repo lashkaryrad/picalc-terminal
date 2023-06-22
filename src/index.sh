27d5075f81a9739b1c5c718379890274309a00de
lashkaryrad <lashkaryrad@gmail.com>
2023-06-22 21:21:05 +0330

    Create API variable

--- src/index.sh
+++ src/index.sh
@@ -4,3 +4,4 @@
 env="../env/.env"
 API_SITE=$( sed "s/#.*//; s/api=*//; s/Key.*.//; s/'//g; /^$/ d" $env ) 
 API_KEY=$( sed "s/#.*//; /https:/ d;s/api....//g; s/'//g; /^$/ d" $env )
+API=$API_SITE"latest?apikey="$API_KEY
