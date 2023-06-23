8acd32efcb3219407555633bd014ad26302e442d
lashkaryrad <lashkaryrad@gmail.com>
2023-06-24 01:07:22 +0330

    init dialog script

--- src/index.sh
+++ src/index.sh
@@ -5,3 +5,6 @@ env="../env/.env"
 API_SITE=$( sed "s/#.*//; s/api=*//; s/Key.*.//; s/'//g; /^$/ d" $env ) 
 API_KEY=$( sed "s/#.*//; /https:/ d;s/api....//g; s/'//g; /^$/ d" $env )
 API=$API_SITE"latest?apikey="$API_KEY
+
+# Dialog
+dialog --backtitle "Forex pip Calculator"  
