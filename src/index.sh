fbd59b488e37e300b3ac3f7e42d9b9cb06c8dc87
lashkaryrad <lashkaryrad@gmail.com>
2023-06-25 03:54:00 +0330

    Create Form

--- src/index.sh
+++ src/index.sh
@@ -9,4 +9,9 @@ API=$API_SITE"latest?apikey="$API_KEY
 # Dialog
 dialog --backtitle "Forex pip Calculator" \
     --title "PIP Calculator" \
-    --form "\nPIP Value Calculator" 25 60 16 \w
+    --form "\nPIP Value Calculator" 25 60 16 \
+        "Pips" 1 1 "$pip" 1 25 25 30 \
+        "Instrument" 2 1 "$instrument" 2 25 25 30 \
+        "Lots" 3 1 "$lot" 3 25 25 30 \
+        "Deposit currency" 4 1 "$deposite_currency" 4 25 25 30 \
+        "1 Pip Size" 4 1 "$pip_size" 4 25 25 30 \
